3ad5425b6e6045ef93822a807ff1a1d4c0b37f95
jshlch <chua.incubixintern@gmail.com>
2019-03-12 19:21:35 +0800

    Folder refractor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from 'App';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// React DevTools Config
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//Store
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware())
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
